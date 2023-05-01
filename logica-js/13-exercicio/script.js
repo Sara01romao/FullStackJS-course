f68d6c6f7f5ddbeac863fa04b616e08269af0b4d
Sara Romão <sara-romao@live.com>
2023-05-01 14:06:30 -0300

    feat: exer2

--- logica-js/13-exercicio/script.js
+++ logica-js/13-exercicio/script.js
@@ -1,3 +1,19 @@
+//1- Escreva um programa em javascript que permita salvar informações de um recruta.
+//As informações a serem salvas são:
+
+/*
+- o primeiro nome
+- o sobrenome
+- o campo de estudo
+- o ano de nascimento
+
+Depois o pragama deve exibir o nome completo do recruta, seu campo de estudo e sua idade
+(apenas baseada no ano de nascimento)
+
+
+*/
+
+
 
 function cadastrar(){
     var nome = document.querySelector('#nome').value;
@@ -26,4 +42,26 @@ function calculaIdade(anoNascimento){
     return ano - anoNascimento;
 
 
+}
+
+
+
+/*2- Escreva um programa em javascript que permita inserir dois valores numéricos e então calcule
+o resultado das quatro operações básicas (soma, subtração, multiplicação e divisão) 
+
+Após calcular os resultados o programa deve exibi-los na tela.
+
+*/
+
+
+
+function calcular(){
+    const num1 = document.getElementById("valor1").value;
+    const num2 = document.getElementById("valor2").value;
+   
+    
+    document.querySelector('.resultado-conta').innerHTML=`
+      Soma  ${parseInt(num1) + parseInt(num2)}; Subtração ${num1 - num2};  Multiplicação ${num1 * num2};
+      Divisão   ${num1 / num2}
+    `
 }
