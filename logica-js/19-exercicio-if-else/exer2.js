a21052332115a7036f6b2d4dd2616f516d88e189
Sara Romão <sara-romao@live.com>
2023-05-06 23:05:30 -0300

    fix:exer

--- logica-js/19-exercicio-if-else/exer2.js
+++ logica-js/19-exercicio-if-else/exer2.js
@@ -11,7 +11,7 @@ se este for o caso)
 const velocidade_veiculo1 = 40;
 const veiculo_nome1= "gol";
 
-const velocidade_veiculo2 = 50;
+const velocidade_veiculo2 = 30;
 const veiculo_nome2= "Parati";
 
 
@@ -27,22 +27,4 @@ if(velocidade_veiculo1 > velocidade_veiculo2){
 
 
 
-/**
- * ## Cálculo de Dano
 
-Escreva um programa que permita inserir o nome e o poder de ataque de um personagem, depois o nome, 
-a quantidade de pontos de vida, o poder de defesa de outro personagem e se ele possui um escudo, e 
-então calcule a quantidade de dano causado baseado nas seguintes regras:
-
-- Se o poder de ataque for maior do que a defesa e o defensor não possuir um escudo, o dano causado 
-será igual a diferença entre o ataque e a defesa.
-- Se o poder de ataque for maior do que a defesa e o defensor possuir um escudo, o dano causado ser
-á igual a metade da diferença entre o ataque e a defesa.
-- Se o poder de ataque for menor ou igual a defesa, o dano causado será 0.
-
-Por fim, o programa deve subtrair a quantidade de dano da quantidade de pontos de vida do personagem 
-defensor e exibir na tela a quantidade de dano e as informações atualizadas de ambos os personagens.
- * 
- * 
- * 
- */
