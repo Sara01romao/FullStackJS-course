3beee7c2da96e870caa7b625400b732988af0adc
Sara Romão <sara-romao@live.com>
2023-05-06 23:06:22 -0300

    switch

---
+++ logica-js/21-switch-case.js
@@ -0,0 +1,25 @@
+
+
+switch("s"){
+    case "a":
+        console.log("O resultado é 'a'");
+    break;
+    
+    case "b":
+        console.log("O resultado é 'b'");
+    break;
+    
+    case "c":
+        console.log("O resultado é 'c'")
+    break
+
+    case "d":
+        
+        console.log("O resultado é 'a'")
+    break
+    
+    default: 
+    
+        console.log("nenhum")
+    //caso padrão 
+}
